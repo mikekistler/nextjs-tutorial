7436d7d31c854303682b25b0bd02d50e2a32273d
Mike Kistler <mikekistler@microsoft.com>
2024-01-05 17:08:53 -0600

    News API hooked up

---
+++ models/NewsArticles.ts
@@ -0,0 +1,15 @@
+export interface NewsArticles {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage?: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface NewsArticlesResponse {
+    status: string;
+    totalResults: number;
+    articles: NewsArticles[];
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import { GetServerSideProps } from 'next';
+import { NewsArticle, NewsArticlesResponse } from '@/models/NewsArticles';
 
-export default function Home() {
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[],
+}
+
+// This function is run on the server -- not in the browser
+export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
+  const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWS_API_KEY);
+  const newsResponse: NewsArticlesResponse = await response.json();
+  return {
+    props: { newsArticles: newsResponse.articles }
+  }
+}
+
+export default function BreakingNewsPage({ newsArticles }: BreakingNewsPageProps) {
   return (
     <>
+      <Head>
+        <title key="title">Breaking News -- NextJS tutorial</title>
+      </Head>
       <main>
-        Hello, world!
+        <h1>Breaking News</h1>
+        {JSON.stringify(newsArticles)}
       </main>
     </>
   )
