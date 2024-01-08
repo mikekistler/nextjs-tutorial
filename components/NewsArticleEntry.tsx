f2301982984ee627aefc78b432411b56f9279c82
Mike Kistler <mikekistler@microsoft.com>
2024-01-07 20:16:20 -0600

    Image optimization

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,5 +1,8 @@
 import { NewsArticle } from '@/models/NewsArticles';
+import Image from 'next/image';
 import { Card } from 'react-bootstrap';
+import placeholderImage from "@/assets/images/newsarticle_placeholder.jpg";
+import styles from '@/styles/NewsArticleEntry.module.css';
 
 interface NewsArticleEntryProps {
     article: NewsArticle,
@@ -12,7 +15,13 @@ const NewsArticleEntry = ({ article : {title, description, url, urlToImage } }: 
         <a href={url}>
             <Card className="h-100"> {/* "h-100" is a Bootstrap class that makes the Card take up the full height of its parent 
                                           https://getbootstrap.com/docs/4.0/utilities/sizing/ */}
-               <Card.Img variant="top" src={validUrlToImage} />
+                <Image
+                    src={validUrlToImage || placeholderImage}
+                    width={500}
+                    height={200}
+                    alt="News Article Image"
+                    className={`card-img-top ${styles.image}`}
+                />
                 <Card.Body>
                      <Card.Title>{title}</Card.Title>
                      <Card.Text>{description}</Card.Text>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.si.com'],
+    remotePatterns: [
+      {
+        // Don't use this in production, this is just an example!
+        protocol: 'https',
+        hostname: '*',
+      },
+      {
+        // Don't use this in production, this is just an example!
+        protocol: 'http',
+        hostname: '*',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
